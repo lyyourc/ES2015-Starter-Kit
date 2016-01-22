96d07e01a5fdbeeaabaf595e9e73d09c12e2ac45
Drake Leung <drakeleung42@gmail.com>
2016-01-23 01:58:20 +0800

    1st commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# ES2015 Starter Kit
+If you just wanna write some SIMPLE ES2015 codes in the browser, TAKE IT~
+
+## Usage
+```
+git clone
+npm install
+npm start
+http://localhost:3000
+```
+For more commands, just check out the `scripts` property of `package.json`.
+
+
+## Debug
+If you wanna debug your un-tranpiled ES2015 code, check this [answer](http://stackoverflow.com/questions/27626764/configure-webpack-to-allow-browser-debugging) out in the stackoverflow.
+
+But it seems a [bug](http://stackoverflow.com/questions/27626764/configure-webpack-to-allow-browser-debugging) exists according to this [issue](http://stackoverflow.com/questions/27626764/configure-webpack-to-allow-browser-debugging).
+
+## Resources
+- [SurviveJS - webpack-react](http://survivejs.com/webpack_react/developing_with_webpack/)
+- [Using ES6 and ES7 in the Browser](http://jamesknelson.com/using-es6-in-the-browser-with-babel-6-and-webpack/)
+- [Babel User Handbook](https://github.com/thejameskyle/babel-handbook/blob/master/translations/en/user-handbook.md#babel-runtime)
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'build')
+};
+
+// common config
+const common = {
+  entry: [
+    'babel-polyfill',
+    PATHS.src
+  ],
+
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'ES2015 Start Kit'
+    })
+  ],
+
+  module: {
+    loaders: [
+      {
+        loaders: ['style', 'css'],
+        test: /\.css$/,
+        include: PATHS.src
+      },
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
+
+// for dev env
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: '#inline-source-map',
+    debug: true,
+
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      host: 'localhost',
+      port: '3000'
+    },
+
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {
+    devtool: 'eval'
+  });
+}
